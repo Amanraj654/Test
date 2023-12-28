6a1a5858dcc705232d7db87ca1b427562fca19fd
Amanraj654 <r61562113+prime@gmail.com>
2023-12-28 17:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def home():
     <h1> <a href="https://mraman.onrender.com">Welcome to My website </a></h1>
     <p style="color:blue;"> This is the first website by Aman using HTML </p>
     <img src="https://graph.org/file/5c0eca2c261e127d7bba1.jpg" alt="Test" style="border-radius: 12px;"/> 
+    <hr> <hr> <br>
     <iframe src="https://www.youtube.com/embed/wTjszS6qFm8">
 </iframe>
 </center>
